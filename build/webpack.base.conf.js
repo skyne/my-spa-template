460977d417a2b2f2b10246b1890f0a8507a7fba2
Skyne <skyne9@gmail.com>
2017-12-06 20:44:23 +0100

    lazyloaded image component and build time webpack svg generation for placeholders

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -66,12 +66,30 @@ module.exports = {
         include: [resolve("src"), resolve("test")]
       },
       {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: "url-loader",
-        options: {
-          limit: 10000,
-          name: utils.assetsPath("img/[name].[hash:7].[ext]")
-        }
+        test: /\.(png|jpe?g|gif)(\?.*)?$/,
+        use: [
+            {
+                loader: 'image-trace-loader',
+                options: {
+                    steps: 10
+                }
+            },
+            {
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath("img/[name].[hash:7].[ext]")
+                }
+            }
+        ]
+      },
+      {
+          test: /\.(svg)(\?.*)?$/,
+          loader: "url-loader",
+          options: {
+              limit: 10000,
+              name: utils.assetsPath("img/[name].[hash:7].[ext]")
+            }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
